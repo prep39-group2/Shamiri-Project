95524f617c8a10d9d482c6605175010f3eb51c03
carolyne <pcmaunda@gmail.com>
2022-05-18 22:09:35 +0300

    added classes in models

--- app/models.py
+++ app/models.py
@@ -40,3 +40,22 @@ class Notes(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Sources:
+    def __init__(self, id,name, description, url, country):
+        self.name=name,
+        self.description=description
+        self.url=url
+        self.id = id
+        self.country = country
+        
+class Articles:
+    '''Define article model'''
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
